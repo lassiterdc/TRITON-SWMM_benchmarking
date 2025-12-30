55b2f03bb844ae953de05d55bdb8c10d723de14c
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2025-12-30 14:20:41 -0500

    format docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TRITON-SWMM_benchmarking'
+project = 'TRITON-SWMM Benchmarking'
 copyright = '2025, Daniel Lassiter'
 author = 'Daniel Lassiter'
 release = '0.1.0'
@@ -14,7 +14,10 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # for Google/NumPy style docstrings
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +27,5 @@ language = 'Python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
